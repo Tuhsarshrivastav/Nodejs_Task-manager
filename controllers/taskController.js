0e858ac2531f046072733a1b53f319180288efe1
Tusharshrivastav <ts3657@gmail.com>
2021-10-18 15:55:04 +0530

    added curd api routes and controllers

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,10 +2,10 @@ module.exports.getAllTasks = (req, res) => {
   res.send("all itms");
 };
 module.exports.createTask = (req, res) => {
-  res.send("all itms");
+  res.json(req.body);
 };
 module.exports.getTask = (req, res) => {
-  res.send("all itms");
+  res.json({ id: req.params.id });
 };
 module.exports.updateTask = (req, res) => {
   res.send("all itms");
