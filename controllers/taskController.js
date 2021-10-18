f2ce8a386446e313b79fd1e7ac0719e0cfed6767
Tusharshrivastav <ts3657@gmail.com>
2021-10-18 18:51:16 +0530

    added taskschema

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,8 +1,14 @@
+const Task = require("../models/TaskModel");
 module.exports.getAllTasks = (req, res) => {
   res.send("all itms");
 };
-module.exports.createTask = (req, res) => {
-  res.json(req.body);
+module.exports.createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    console.log(error.stack);
+  }
 };
 module.exports.getTask = (req, res) => {
   res.json({ id: req.params.id });
